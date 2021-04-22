43296336a3a6d8082647983d767fd60342fd5a83
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-04-22 12:03:54 -0300

    [FIX] fix 500 error

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+x8q!1rsmo61i64tm+5sx8bml=(=q8qeo=xi1aa%5sm-_!#84e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://connect-sigaa-server.herokuapp.com','https://connect-sigaa-server.herokuapp.com/']
+ALLOWED_HOSTS = ['https://connect-sigaa-server.herokuapp.com','https://connect-sigaa-server.herokuapp.com/*', '*']
 
 # Application definition
 
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
