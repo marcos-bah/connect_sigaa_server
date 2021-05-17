f510642b90a8db08587169f6f3e255958145d6dd
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-05-17 09:18:23 -0300

    [DEPLOY] alterando para deploy

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'https://connect-sigaa-server.herokuapp.com']
 
